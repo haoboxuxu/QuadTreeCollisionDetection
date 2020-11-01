9d172854ac70eac97dc22a34b6fca424c6b38d84
徐浩博 <xuhaobo@xuhaobodeMacBook-Pro.local>
2020-11-01 17:58:19 +0800

    add visualization

--- quadtree.js
+++ quadtree.js
@@ -1,38 +1,63 @@
 class QuadTree {
-	constructor(bounds, cap) {
-		this.bounds = bounds;
-		this.cap = cap;
-		this.childPoints = [];
+  constructor(bounds, cap) {
+    this.bounds = bounds;
+    this.cap = cap;
+    this.childPoints = [];
     this.isDivided = false;
-	}
+  }
 
-	insert(point) {
-    if (this.childPoints.length < this.cap) {
-      this.childPoints.push(point);
-    }else {
-          if (!this.isDivided) {
-            this.subDivide(); 
-            this.isDivided = true;
-          }
-        }
+  insert(point) {
+  if (this.childPoints.length < this.cap) {
+     this.childPoints.push(point);
+  }else {
+      if (!this.isDivided) {
+        this.subDivide();
+      }
+      this.topRight.insert(point);
+      this.topLeft.insert(point);
+      this.buttomRight.insert(point);
+      this.buttomLeft.insert(point);
+    }
   }
     
-    subDivide() {
-      let x = this.bounds.x;
-      let y = this.bounds.y;
-      let w = this.bounds.w / 2;
-      let h = this.bounds.h / 2;
+  subDivide() {
+    let x = this.bounds.x;
+    let y = this.bounds.y;
+    let w = this.bounds.w / 2;
+    let h = this.bounds.h / 2;
+    
+    let tr = new Rectangle(x + w, y - h, w, h);
+    this.topRight = new QuadTree(tr, this.cap);
+    
+    let tl = new Rectangle(x - w, y - h, w, h);
+    this.topLeft = new QuadTree(tl, this.cap);
+    
+    let br = new Rectangle(x + w, y + h, w, h);
+    this.buttomRight = new QuadTree(br, this.cap);
       
-      let tr = new Rectangle(x + w, y - h, w, h);
-      this.topRight = new QuadTree(tr, this.cap);
+    let bl = new Rectangle(x - w, y + h, w, h);
+    this.buttomLeft = new QuadTree(bl, this.cap);
       
-      let tl = new Rectangle(x - w, y - h, w, h);
-      this.topRight = new QuadTree(tl, this.cap);
-      
-      let br = new Rectangle(x + w, y + h, w, h);
-      this.topRight = new QuadTree(br, this.cap);
-      
-      let bl = new Rectangle(x - w, y + h, w, h);
-      this.topRight = new QuadTree(bl, this.cap);
+    this.isDivided = true;
+  }
+  
+  show() {
+    stroke(255);
+    noFill();
+    strokeWeight(1);
+    rectMode(CENTER);
+    rect(this.bounds.x, this.bounds.y, this.bounds.w * 2, this.bounds.h * 2);
+    if (this.isDivided) {
+      this.topRight.show();
+      this.topLeft.show();
+      this.buttomRight.show();
+      this.buttomLeft.show();
     }
+    for (let p of this.childPoints) {
+      console.log(p.x + " " + p.y);
+      strokeWeight(5);
+      point(p.x, p.y);
+    }
+  }
+  
 }
--- sketch.js
+++ sketch.js
@@ -1,17 +1,20 @@
 function setup() {
-	createCanvas(800, 800);
-	let bounds = new Rectangle(400, 400, 400, 400);
-	let cap = 4;
-	let quadTree = new QuadTree(bounds, cap);
+  createCanvas(800, 800);
+  let bounds = new Rectangle(400, 400, 400, 400);
+  let cap = 4;
+  let quadTree = new QuadTree(bounds, cap);
 
-	for (let i = 0; i < 50; i++) {
-		let point = new Point(random(width), random(height));
-		quadTree.insert(point);
-	}
-	let treeDFS = new TresDFS();
-    treeDFS.dfs(quadTree);
+  for (let i = 0; i < 10; i++) {
+    let point = new Point(random(width), random(height));
+    quadTree.insert(point);
+  }
+  
+  // let treeDFS = new TresDFS();
+  // treeDFS.dfs(quadTree);
+  background(0);
+  quadTree.show();
 }
 
 function draw() {
-  background(220);
+  //background(220);
 }
